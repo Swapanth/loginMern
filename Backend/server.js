67a84e3d75831aa923d6ffa1661face464e75323
Swapanth <swapanthvakapalli@gmail.com>
2024-04-22 15:14:56 +0530

    backend just started

--- Backend/server.js
+++ Backend/server.js
@@ -1,1 +1,14 @@
-?////
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = 7777;
+
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
